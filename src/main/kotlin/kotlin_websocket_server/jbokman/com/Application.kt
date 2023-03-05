cfb9a7cb4a033adb8c23ce75b3c5b8bf96f51f4e
Julian Bokelmann <j.b.bokelmann+github@gmail.com>
2023-03-05 13:48:12 +0100

    ✨ change to embedded server

--- src/main/kotlin/kotlin_websocket_server/jbokman/com/Application.kt
+++ src/main/kotlin/kotlin_websocket_server/jbokman/com/Application.kt
@@ -1,13 +1,32 @@
 package kotlin_websocket_server.jbokman.com
 
 import io.ktor.server.application.*
-import kotlin_websocket_server.jbokman.com.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 12345, module = Application::myApplicationModule).start(wait = true)
+}
 
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-fun Application.module() {
-    configureSockets()
-    configureRouting()
+fun Application.myApplicationModule() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        webSocket("/") {
+            send("You are connected!")
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                send("You said: $receivedText")
+            }
+        }
+    }
 }
